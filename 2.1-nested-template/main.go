3e23f97dceea9c962f64fae50760b3637ba08b37
Domen Skamlic <domen@skamlic.com>
2020-09-11 12:04:50 +0200

    nested template example

---
+++ 2.1-nested-template/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"html/template"
+	"net/http"
+)
+
+var homeView *template.Template
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	err := homeView.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	var err error
+	homeView, err = template.ParseFiles("home.gohtml", "footer.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	http.ListenAndServe(":3000", r)
+}
