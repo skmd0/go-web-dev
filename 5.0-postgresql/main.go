49ae01121854c8c418d03ce0b06b7ba9176b2388
Domen Skamlic <domen@skamlic.com>
2020-09-17 14:21:32 +0200

    add postgresql example

---
+++ 5.0-postgresql/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "testtest"
+	dbName   = "postgres"
+)
+
+func main() {
+	var dbConnection string
+	dbConnection = fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable",
+		host, port, user, password, dbName)
+	db, err := sql.Open("postgres", dbConnection)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	err = db.Close()
+	if err != nil {
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
-	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/gorilla/schema v1.2.0
+	github.com/lib/pq v1.8.0
 )
