c9f722efc8a157bb6275de1056d117b80de18276
Domen Skamlic <domen@skamlic.com>
2020-11-09 11:23:44 +0100

    add httponly cookie example

---
+++ cookie/disable_js/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func createCookie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	cookie := http.Cookie{
+		Name:  "email",
+		Value: "domenko@skamlici.com",
+		// This line disables the access to the cookie via JS
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &cookie)
+	fmt.Fprintln(w, "Cookie created! <a href='/read'>See the cookie</a>")
+}
+
+func readCookie(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("email")
+	if err != nil {
+		fmt.Fprintln(w, "Unable to read cookie.")
+		return
+	}
+	fmt.Fprintln(w, cookie)
+}
+
+func main() {
+	http.HandleFunc("/", createCookie)
+	http.HandleFunc("/read", readCookie)
+	http.ListenAndServe(":3000", nil)
+}
